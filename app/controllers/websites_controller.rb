0410cbb9d998ea327fa6d6f4c409fe05e3662e74
Tom Fafard <tfafard@Toms-MacBook-Pro.local>
2016-02-15 20:05:43 -0500

    created info box

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -2,4 +2,26 @@ class WebsitesController < ApplicationController
   def index
 
   end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      flash[:notice] = "Your lead has been saved to the database."
+      redirect_to root_path
+    else
+      flash[:errors] = @message.errors.full_messages.join(". ")
+      render :index
+    end
+  end
+
+  protected
+
+  def message_params
+    params.require(:message).permit(:name, :company, :email, :note)
+  end
 end
