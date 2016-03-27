15d20b9dbeb30e751246bcb6407e3067aaab10a3
Tom Fafard <tfafard@Toms-MacBook-Pro.local>
2016-03-27 15:29:33 -0400

    fix hound violations

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -1,6 +1,10 @@
 class Blogpost < ActiveRecord::Base
-  has_attached_file :image, :styles => { :normal => "600x400" },
-                    :default_style => :normal
+  has_attached_file :image, styles: { normal: "600x400" },
+                    default_style: :normal
 
-  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates_attachment :image,
+  content_type: { content_type: ["image/jpg",
+                                "image/jpeg",
+                                "image/png",
+                                "image/gif"] }
 end
